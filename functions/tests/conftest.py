67cd4d949bbc0db13381eb2a044038e7aa6103e6
tommykey0925 <tommykey0925@gmail.com>
2026-04-06 18:26:26 +0900

    Clean up unused infra: remove Textract-era resources
    - Remove unused VPC/subnet data sources (leftover from EKS project)
    - Remove budget-checker + notifier from Lambda pipeline (moved to API)
    - Remove us-east-1 S3 bucket + cross-region replication (Textract legacy)
    - Remove RECEIPTS_BUCKET_US env var from Lambda + tests
    - Update CD to deploy only 4 pipeline Lambdas

--- functions/tests/conftest.py
+++ functions/tests/conftest.py
@@ -15,7 +15,6 @@ os.environ["AWS_ACCESS_KEY_ID"] = "testing"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
 os.environ["AWS_SECURITY_TOKEN"] = "testing"
 os.environ["AWS_SESSION_TOKEN"] = "testing"
-os.environ["RECEIPTS_BUCKET_US"] = "test-receipts-us"
 
 
 @pytest.fixture()
